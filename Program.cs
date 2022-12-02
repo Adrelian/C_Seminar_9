d2dde957469775a59f72f57a69554d962c94abcc
Anatolii <anatolii.polovnikov@gmail.com>
2022-12-02 22:50:52 +0500

    Сделал красивый ввод и вывод

--- Program.cs
+++ Program.cs
@@ -5,14 +5,35 @@
 //N = 5 -> "1, 2, 3, 4, 5"
 //N = 6 -> "1, 2, 3, 4, 5, 6"
 
-void allNaturalNumber (int N)
-{
-    if (N >= 1) // условие окончание рекурсии
-    {
-        allNaturalNumber(N - 1); // на вход рекурсии нельзя давать тоже число что в методе 
+// void allNaturalNumber (int N)
+// {
+//     if (N >= 1) // условие окончание рекурсии
+//     {
+//         allNaturalNumber(N - 1); // на вход рекурсии нельзя давать тоже число что в методе 
         
-        Console.Write(N + " ");     
+//         Console.Write(N + " ");     
+//     }
+// }
+
+// allNaturalNumber(10);
+
+//Задание 2. Напишите программу,
+// которая будет принимать на вход число и возвращать сумму его цифр.
+
+//453 -> 12
+//45 -> 9
+
+int sumNumbers(int num)
+{
+
+    if (num != 0)
+    {
+        return sumNumbers(num / 10) + num % 10;
     }
+    return num;  
 }
 
-allNaturalNumber(10);
+Console.WriteLine("Введите число ");
+int num = Convert.ToInt32(Console.ReadLine());
+int sum = sumNumbers(Math.Abs(num));
+Console.WriteLine($"Сумма цифр числа {num} равна {sum}");
